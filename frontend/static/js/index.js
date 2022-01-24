209b8999c8e945f03a15efc14416161bf9ce5acc
carlos <carlosramos@sime.es>
2022-01-24 11:46:37 +0100

    first commit

---
+++ frontend/static/js/index.js
@@ -0,0 +1,41 @@
+const navigateTo = url => {
+    history.pushState(null, null, url);
+    router();
+}
+
+const router = async () => {
+    const routes = [
+        { path: '/', vivew: () => console.log('Viewing Dashboard') },
+        { path: '/posts', vivew: () => console.log('Viewing Posts') },
+        { path: '/settings', vivew: () => console.log('Viewing Settings') },
+    ];
+
+    // Test each route for potential match
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route, 
+            isMatch: location.pathname === route.path
+        }
+    });
+
+    let match = potentialMatches.find(potentialMatche => potentialMatche.isMatch);
+
+    if (!match) { // Go to default page or error page.
+        match = {
+            route: routes[0],
+            isMatch: true
+        };
+    }
+    console.log(match.route.vivew());
+};
+
+document.addEventListener('DOMContentLoaded', () =>{
+    document.body.addEventListener('click', e => {
+        if (e.target.matches('[data-link]')) {
+            console.log(e.target.href);
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    })
+    router();
+});
